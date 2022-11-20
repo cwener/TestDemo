5caa47f318983d18f4ea2a52e5eef6a242c095a3
chengwen <chengwen02@corp.netease.com>
2022-11-20 10:49:06 +0800

    最长无重复子串

---
+++ app/src/main/java/com/example/test/leetcode/LengthOfLongestSubstring.java
@@ -0,0 +1,27 @@
+package com.example.test.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: chengwen
+ * @date: 2022/11/17
+ */
+public class LengthOfLongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        if(s == null || s.length() == 0) return 0;
+        Map<Character, Integer> map = new HashMap<>();
+        int maxSub = 0;
+        int left = 0, right = 0;
+        while(right < s.length()) {
+            if(map.containsKey(s.charAt(right))) {
+                left = map.get(s.charAt(right)) + 1;
+            }
+            maxSub = Math.max(maxSub, right - left + 1);
+
+            map.put(s.charAt(right), right);
+            right++;
+        }
+        return maxSub;
+    }
+}
---
+++ app/src/main/java/com/example/test/leetcode/Main.java
@@ -0,0 +1,11 @@
+package com.example.test.leetcode;
+
+/**
+ * @author: chengwen
+ * @date: 2022/11/17
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
